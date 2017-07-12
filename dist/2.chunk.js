019e51369acd7f39fe0cfe0ab52ff662d3e6eb24
administrator <liquidnuker@users.noreply.github.com>
2017-07-12 09:07:30 -0700

    added Carousel1-01 items

--- dist/2.chunk.js
+++ dist/2.chunk.js
@@ -1,1 +1,110 @@
-webpackJsonp([2],{4:function(e,n,t){var o=t(3)(t(8),t(9),null,null);e.exports=o.exports},8:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={data:function(){return{}},mounted:function(){console.log("Carousel1_01.vue mounted")}}},9:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("div")},staticRenderFns:[]}}});
+webpackJsonp([2],{
+
+/***/ 18:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return store; });
+var store = {
+  items: [{
+    itemName: "item1",
+    description: "",
+    imgSrc: ""
+  }, {
+    itemName: "item2",
+    description: "",
+    imgSrc: ""
+  }, {
+    itemName: "item3",
+    description: "",
+    imgSrc: ""
+  }]
+};
+
+
+
+/***/ }),
+
+/***/ 4:
+/***/ (function(module, exports, __webpack_require__) {
+
+var Component = __webpack_require__(3)(
+  /* script */
+  __webpack_require__(8),
+  /* template */
+  __webpack_require__(9),
+  /* scopeId */
+  null,
+  /* cssModules */
+  null
+)
+Component.options.__file = "D:\\WINDOWS\\GD2\\web\\testfiles\\webpack2\\_node_dev\\git_vueCarousel1\\src\\vue-components\\Carousel1_01.vue"
+if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
+if (Component.options.functional) {console.error("[vue-loader] Carousel1_01.vue: functional components are not supported with templates, they should use render functions.")}
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-2ebc8f98", Component.options)
+  } else {
+    hotAPI.reload("data-v-2ebc8f98", Component.options)
+  }
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ 8:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__js_store_js__ = __webpack_require__(18);
+//
+//
+//
+//
+//
+//
+//
+
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      items: __WEBPACK_IMPORTED_MODULE_0__js_store_js__["a" /* store */].items,
+      cIndex: 0
+    };
+  },
+
+  mounted: function mounted() {
+    console.log("Carousel1_01.vue mounted");
+    // console.log(this.cIndex);
+  }
+});
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', [_c('div', [_vm._v("\n    " + _vm._s(_vm.items[_vm.cIndex].itemName) + "\n  ")])])
+},staticRenderFns: []}
+module.exports.render._withStripped = true
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+     require("vue-hot-reload-api").rerender("data-v-2ebc8f98", module.exports)
+  }
+}
+
+/***/ })
+
+});
--- src/vue-components/Carousel1_01.vue
+++ src/vue-components/Carousel1_01.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
+    <div>
+      {{ items[cIndex].itemName }}
+    </div>
   </div>
 </template>
 <script>
+import {store} from "../js/store.js";
   export default {
     data () {
       return {
+        items: store.items,
+        cIndex: 0
       }
     },
     mounted: function () {
       console.log("Carousel1_01.vue mounted");
+      // console.log(this.cIndex);
     } 
   }
 </script>
